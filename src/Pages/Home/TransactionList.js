9a9a760320f699e9b29e3db7aa40029c55e7e595
jalanprakhar <iit2020200@iiita.ac.in>
2022-02-20 23:22:04 +0530

    Added total ammount functionality

--- src/Pages/Home/TransactionList.js
+++ src/Pages/Home/TransactionList.js
@@ -1,12 +1,19 @@
 import { useFirestore } from '../../hooks/useFirestore'
 import styles from './Home.module.css'
 export default function TransactionList({transactions}) {
-
+    let total=0;
+    // console.log(transactions);
+    transactions.map(t=>(
+      total+= +t.amount
+    ))
+    console.log('total ' ,total);
     const {deleteDocument,response}=useFirestore('transactions');
     console.log(response);
 
     console.log(transactions)
   return (
+    <>
+    {total>0 && <h2>Total amount spent till now : {total}</h2>}
     <ul className={styles.transactions}>
         {transactions.map(t=>(
             <li key={t.id}>
@@ -15,6 +22,6 @@ export default function TransactionList({transactions}) {
                 <button onClick={()=>deleteDocument(t.id)}>x</button>
             </li>
         ))}
-    </ul>
+    </ul></>
   )
 }
